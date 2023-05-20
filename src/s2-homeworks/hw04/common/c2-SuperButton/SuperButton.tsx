4048b7a74206ea6ab2e314c4b50d6020aaf6ec70
Maksym Vedmedenko <maksym.vedmedenko@gmail.com>
2023-05-19 21:45:21 -0400

    hw4 done

--- src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
+++ src/s2-homeworks/hw04/common/c2-SuperButton/SuperButton.tsx
@@ -18,11 +18,9 @@ const SuperButton: React.FC<SuperButtonPropsType> = (
     }
 ) => {
     const finalClassName = s.button
-        // + (disabled
-        //         ? ...
-        //         : xType === 'red'
-        //             ? ...
-        + (className ? ' ' + className : '') // задачка на смешивание классов
+        + (disabled ? ' ' + s.disabled : xType === 'red'
+            ? ' ' + s.red : xType === 'secondary'
+                ? ' ' + s.secondary : ' ' + s.default) // задачка на смешивание классов
 
     return (
         <button
